650848b045e5eef8ac1b55d1da0f59c936e619e0
Spencer59 <104880918+Spencer59@users.noreply.github.com>
2024-06-20 14:40:52 -0700

    Create UI main.lua

---
+++ UI main.lua
@@ -0,0 +1,29 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Gym Leauge Hub",
+   LoadingTitle = "Gym Leauge Interface Suite",
+   LoadingSubtitle = "by Spen599",
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+local Tab = Window:CreateTab("Main", 4483362458)
+local Button = Tab:CreateButton({
+   Name = "Auto Farm",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
+})
