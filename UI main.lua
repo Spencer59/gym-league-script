e28c89849d6adb70a33d46345e35d03b4f0cfd8c
Spencer59 <sppj6789@outlook.com>
2024-06-22 00:41:52 +0200

    Update UI main.lua

--- UI main.lua
+++ UI main.lua
@@ -129,9 +129,13 @@ while _G().New_Gym == true do
     print("This is awsome!") -- will fire the teliport button on the gym gui
 end
 ]]
+if unlock_gym_text == true then
+    _G().New_Gym = true
+end
 
-
-
+while _G().New_Gym == true do
+    print("This is awsome!") -- will fire the teliport button on the gym gui
+end
 
 
 local Tab = Window:CreateTab("Main", 4483362458)
@@ -171,7 +175,7 @@ local Toggle = Tab:CreateToggle({
 local Button = Tab:CreateButton({
     Name = "Load Litux",
     Callback = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/cool83birdcarfly02six/LightuxSolaraSup/main/README.md'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/cool83birdcarfly02six/LightuxSolaraSup/main/README.md'))()
     -- The function that takes place when the button is pressed
     end,
  })
